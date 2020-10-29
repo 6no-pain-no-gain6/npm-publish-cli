da7d354020cfcc31a34a321cc5efce99fda9c2b3
duxuyong <1742815841@qq.com>
2020-10-29 16:02:26 +0800

    增加按钮单文件

---
+++ src/components/DButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div >
+    <button @click="handleC">{{text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    text:{
+      type:String,
+      default(){return '按钮'}
+    },
+    handleC:{
+      type:Function,
+      default(){
+        return function(){
+          console.log('按钮被点击了')
+        }
+      }
+    }
+  },
+  name:'DButton',
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+
+  },
+
+  mounted () {
+    window.app=this
+  }
+
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
